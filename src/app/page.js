bcef4c5f8c1aea4d2693427a28caf4156cfb4a45
Mirza Sahin <salihmirza123@gmail.com>
2023-05-26 11:25:41 +0300

    Add next.config.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,25 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+
+export function getStaticProps({ locale }) {
+  return {
+    props: {
+      title: "Başlık",
+      // messages: {
+      //   ...require(`../messages/shared/${locale}.json`),
+      //   ...require(`../messages/index/${locale}.json`),
+      // },
+    },
+  }
+}
+
+export default function Home({title}) {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          {title}&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
